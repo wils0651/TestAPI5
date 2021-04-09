79ce59b45861d50debd44f9bcdc59c9caac2509b
wils0651 <wils0651@hotmail.com>
2021-04-08 21:54:24 -0600

    initial commit.

---
+++ TestAPI5/Controllers/StatusController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI5.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetStatus()
+        {
+            return "true";
+        }
+    }
+}

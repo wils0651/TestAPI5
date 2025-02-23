0044fde14f6fe2fe04691b485c32ffeca31a065f
Tim Wilson <tim@tim.com>
2025-02-22 17:06:30 -0700

    Change logging

--- TestAPI5/Models/TemperatureStatistic.cs
+++ TestAPI5/Models/TemperatureStatistic.cs
@@ -1,6 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TestAPI5.Models
 {
--- TestAPI5/Program.cs
+++ TestAPI5/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
 
 namespace TestAPI5
 {
@@ -12,7 +15,18 @@ namespace TestAPI5
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+                    var env = Environment.GetEnvironmentVariable("APP_ENV");
+                    if (!string.IsNullOrEmpty(env))
+                    {
+                        config.AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true);
+                    }
+                })
+            .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
